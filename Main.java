7e2afd1f82e30bdb289800ddb579c3f1c832d145
Josquin Larsen <jsqnlrsn@gmail.com>
2026-04-07 14:50:43 -0400

    generic static array

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) throws StaticArrayException {
+        StaticArray<Integer> sa = new StaticArray<>();
+        for (int i = 0; i < sa.length(); i++) {
+            sa.set(i, i + 1);
+        }
+
+        String[] fruits = {"apple", "banana", "kumquat"};
+        StaticArray<String> sa2 = new StaticArray<>();
+        for (int i = 0; i < 3; i++) {
+            sa2.set(i, fruits[i]);
+        }
+
+        System.out.println(sa);
+        System.out.println(sa2);
+    }
+
+}
---
+++ StaticArray.java
@@ -0,0 +1,71 @@
+class StaticArrayException extends Exception {
+    public StaticArrayException(String message) {
+        super(message);
+    }
+}
+
+class InvalidIndexException extends StaticArrayException {
+    public InvalidIndexException(String message) {
+        super(message);
+    }
+}
+
+class EmptyArrayException extends StaticArrayException {
+    public EmptyArrayException(String message) {
+        super(message);
+    }
+}
+
+public class StaticArray<T> {
+    // T = Generic, to handle flexible types
+    private T[] data;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public StaticArray(int capacity) throws StaticArrayException {
+        if (capacity < 1) { 
+            throw new StaticArrayException("Capacity must be a positive integer"); 
+        }
+        // cast object to T (workaround)
+        data = (T[]) new Object[capacity];
+        size = capacity;
+    }
+
+    @SuppressWarnings("unchecked")
+    public StaticArray() {
+        data = (T[]) new Object[10];
+        size = 10;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("StaticArray: [");
+        for (int i = 0; i < size; i++ ) {
+            sb.append(data[i]);
+            if (i < size - 1) sb.append(", ");
+        }
+        sb.append("]");
+
+        return sb.toString();
+    }
+
+    public int length() {
+        return size;
+    }
+
+    public T get(int index) throws InvalidIndexException{
+        if (index < 0 || index > size - 1 ) {
+            throw new InvalidIndexException("Index out of bounds");
+        }
+        return data[index];
+    }
+
+    public void set(int index, T item) throws InvalidIndexException {        
+        if (index < 0 || index > size - 1 ) {
+            throw new InvalidIndexException("Index out of bounds");
+        }
+        data[index] = item;
+    }
+
+
+}
